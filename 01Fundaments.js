9ca2a7fb025f89556a8901cd40f7ff08e344d6d1
sachin <nagrajsachin02@gmail.com>
2023-12-08 19:46:17 +0530

    Inprogress

--- 01Fundaments.js
+++ 01Fundaments.js
@@ -72,7 +72,131 @@
 // Equality Operators (==, ===)
 // Logical Operators (||, &&, !)
 
-// 6. Closures
+// 6. Function - A piece of code that we can reuse over and over again in our code. (DRY Principle)
+
+// 6.1 Function declaration - function can be invoked before it's declaration. Hosting allows this.
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+// 6.2 Function expression - Essentially a function value stored in a variable. Function cannot be invoked before declaration, Hosting doesn't allow because variables are hoisted as undefined before initialization.
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+// 6.3 Arrow function - Great for a quick one-line functions. (Has no this keyword)
+const calcAge3 = birthYear => 2037 - birthYear;
+
+// 6.4 Default Parameters
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+
+    bookings.push(booking); // [{ flightNum: 'LH123', numPassengers: 1, price: 199}]
+}
+
+createBooking('LH123');
+
+// 6.5 First Class Functions - store functions in variables / properties or Pass functions as arguments to OTHER functions
+const add = (a, b) => a + b;
+
+const counter = {
+    value: 23,
+    inc: function () { this.value++; }
+}
+
+const greet = () => console.log('Hey Jonas');
+btnClose.addEventListener('click', greet);
+
+// 6.6 Higher Order Functions - function that receives another function / function that returns new function
+function count() {  // Higher Order Function
+    let counter = 0;
+    return function() { // Returned Function
+        counter++;
+    }
+}
+
+// 6.7 Functions Accepting Callback Functions
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher Order Function
+const transformer = function (str, fn) {
+    console.log(`Original String: ${str}`);
+    console.log(`Transformed String: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+
+// 6.8 Function returning function
+const greeting = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+const greetHey = greeting('Hey');
+greetHey('Jonas');
+greetHey('Steve');
+
+greeting('Hey')('Sachin');
+
+// 6.9 Call / Apply / Bind Methods
+// Call - The call method is basically used to invoke the function with different this object.
+// Apply - Just like the call method we can also bind the function to any object. Accepts aruguments as an array.
+// Bind - The bind() method creates a new function and when that new function is called, it set this keyword to the first argument which is passed to the bind method.
+//        sequences of arguments are passed to the new function when the new function is called.
+
+const luftansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book: function (flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+    }
+}
+
+luftansa.book(239, 'Jonas');
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: []
+};
+
+const book = luftansa.book;
+
+// Does not work
+book(23, 'Sarah');
+
+// Call Method
+book.call(eurowings, 23, 'Sarah');
+
+// Apply Method
+const flightData = [583, 'George'];
+book.apply(eurowings, flightData);
+
+// Bind Method
+const bookEW = book.bind(eurowings);
+bookEW(64, 'James Bond');
+
+// 6.10 Immediately Invoked Function Expressions (IIFE)
+// Regular Function 
+(function () {
+    console.log('This function will never run again');
+})();
+
+// Arrow Function
+(() => console.log('This function will never run again'))();
+
+// 6.11. Closures
 // A function has access to the variable environment (VE) of the execution context in which it was created, even after that execution context is gone. 
 // Closure: VE attached to the function, exactly as it was at the time and place the function was created.
 
@@ -135,30 +259,180 @@ const boardPassengers = function (n, wait) {
 
 boardPassengers(180, 3);
 
-// 7. Function - A piece of code that we can reuse over and over again in our code. (DRY Principle)
+// 7. Data Structures: Arrays, Objects, maps, and sets (https://programmingwithmosh.com/javascript/js-objects-arrays-maps-and-sets/)
 
-// Function declaration - function can be invoked before it's declaration. Hosting allows this.
-function calcAge1(birthYear) {
-    return 2037 - birthYear;
+// 7.1 Object - An object is basically a collection of key-value pairs. It can contain properties and methods.
+
+// Example - 1
+const person = { 
+    name: 'John Doe', 
+    city: 'San Francisco', 
+    age: 28,
+    fullName: function () {
+        return `${this.name} is the full name`;
+    }
+};
+
+// 7.2 Map - Maps also allow us to create a collection of key-value pairs. To create a map instance, you need to use the Map constructor. 
+// Similarly, all operations that can be performed on a map are available as methods, such as set, get, has and forEach.
+
+const objectKey1 = {};
+const objectKey2 = 321;
+
+const map = new Map();
+map.set(objectKey1, 'this is the value of the first item');
+map.set(objectKey2, 'this is the value of the second item');
+
+// retrieving the values of a map is quite easy
+console.log(map.get(objectKey1)); // => 'this is the value of the first item'
+console.log(map.get(objectKey2)); // => 'this is the value of the second item'
+
+// Maps are also iteration-friendly
+map.forEach((value, key) => console.log('Key:', key, 'value:', value));
+// => key: {}, value: this is the value of the first item
+// => key: 321, value: this is the value of the second item
+
+// pros: 
+// Maps are easy to iterate over. In order to do so, a map instance provides methods like forEach(), entries(), keys() and values().
+// Maps allow you to use virtually anything as a key: strings, numbers, symbols, objects, booleans and so on and so forth.
+
+// 7.3 Array - arrays are collections whose values are indexed by positive numbers, starting from zero
+
+const colors1 = ['red', 'blue', 'black', 'green', 'white'];
+console.log(colors1[0]); //=> "red"
+console.log(colors1[2]); //=> "black"
+
+// 7.4 Set - Sets are not meant to be a replacement for arrays. In fact, these two types of collections have a number of significant differences.
+
+const color2 = new Set(['red', 'green', 'blue', 'red', 'blue']);
+console.log(colors2); //=> Set(3) {"red", "green", "blue"}
+
+// As you can see, we tried to instantiate a set with repeated values, but this is simply not possible. Sets ensure uniqueness of values.
+
+// Sets also won’t allow you to access an item directly. If you know which value you’re looking for, you can check if a set contains it, 
+// by using the .has() method, or you can even delete this value easily, by using delete(). But if you want to find, let’s say, 
+// the third item of a set (following the insertion order), you have only two options:
+
+// Iterating over it, with the forEach method or by using the iterators that are returned by the .keys()/.values() methods.
+// Converting the set to an array and handling it however you want. 
+// Please note that, under the hood, the JS engine iterates over the map anyway in order to produce the output array.
+
+// Conclusion
+// Plain objects are the right choice when string (or symbol) keys are enough for you.
+// Maps are the way to go if you want to create an association between any two data types.
+// Arrays are the ideal option when direct access to items is crucial and uniqueness of values is not required.
+// Sets are useful when you want to ensure that values don’t repeat in a same instance.
+
+// 8. ES6 Features - https://diversedaily.com/es6-features-arrow-functions-template-literals-destructuring-spread-rest-operators-and-more/
+
+// 8.1 Arrow functions: They have a shorter syntax compared to traditional function expressions and lexically bind the this value.
+// Arrow functions are especially useful when working with higher-order functions and callbacks.
+
+const sum = (a, b) => a + b;
+console.log(sum(2, 3)); // Output: 5
+
+// 8.2 Template Literals: Template Literals allow for easy string interpolation and multiline strings in JavaScript.
+// They are enclosed by backticks (`) instead of single or double quotes.
+
+const name = 'John';
+const message = `Hello, ${name}!
+This is a multiline string.`;
+console.log(message);
+
+// Template literals support placeholders (${expression}) that can include variables, expressions, or function calls
+// making string manipulation more convenient.
+
+// 8.3 Destructuring: Destructuring enables you to extract values from arrays or objects into individual variables. It provides a concise 
+// syntax to unpack values.
+
+// Array Destructuring
+const numbers = [1, 2, 3];
+const [a, b, c] = numbers;
+console.log(a, b, c); // Output: 1 2 3
+
+// Object Destructuring
+const person1 = { firstname: 'John', age: 30 };
+const { firstname, age } = person1;
+console.log(name, age); // Output: John 30
+
+// 8.4 Spread / Rest operators: The spread operator (...) and rest parameters provide convenient ways to work with arrays and function arguments.
+// The spread operator expands an array into individual elements, while the rest parameter collects multiple function arguments into an array.
+
+// Spread Operator
+const numerics = [1, 2, 3];
+const expanded = [...numerics, 4, 5];
+console.log(expanded); // Output: [1, 2, 3, 4, 5]
+
+// Rest Parameter
+function sum(...numbers) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
-// Function expression - Essentially a function value stored in a variable. Function cannot be invoked before declaration, Hosting doesn't allow because variables are hoisted as undefined before initialization.
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
+console.log(sum(1, 2, 3, 4)); // Output: 10
 
-// Arrow function - Great for a quick one-line functions. (Has no this keyword)
-const calcAge3 = birthYear => 2037 - birthYear;
+// 9. Functional Programming
 
-// Default Parameters
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-    const booking = {
-        flightNum,
-        numPassengers,
-        price,
-    };
+// map: This method is used to apply a particular operation on each element of the array in a callback function and then return the update array.
+// map() creates a new array from calling a function for every array element
+// map() does not execute the function for empty elements
+// map() does not change the original array
 
-    bookings.push(booking); // [{ flightNum: 'LH123', numPassengers: 1, price: 199}]
-}
+var arr = [2, 4, 8, 10];
+var updartedArr = arr.map(val => val + 2);
+console.log(updartedArr); // Output: [4, 6, 10, 12]
 
-createBooking('LH123');
+// filter: This method is used to return an array that contains the elements which satisfy the condition applied inside the callback function.
+// filter() method creates a new array filled with elements that pass a test provided by a function
+// filter() does not execute the function for empty elements
+// filter() does not change the original array
+
+var arr= [2,4,8,10] 
+var updatedArr = arr.filter(val=> val<5) 
+console.log(updatedArr); // Output: [2, 4]
+
+// reduce: This method is used to apply a callback function to each element in an array and return a single element
+// reduce() method executes a reducer function for array element
+// reduce() method returns a single value: the function's accumulated result
+// reduce() does not execute the function for empty elements
+// reduce() does not change the original array
+
+var arr= [2,4,8,10] 
+var updatedArr = arr.reduce((prev, curr)=> curr= prev+curr) 
+console.log(updatedArr); // Output: 24
+
+// 10. JSON
+
+// JSON.parse() - takes a JSON string and transforms it into a javascript object
+
+let userString1 = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
+
+let userObj = JSON.parse(userString1);
+
+console.log(userObj); // Output: { name: 'Sammy', email: 'sammy@example.com', plan: 'Pro' }
+
+// JSON.parse() can take a function as a second argument that can transform the object values before they are returned.
+
+let userString2 = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
+
+let userObject1 = JSON.parse(userString2, (key, value) => {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  }
+  return value;
+});
+
+console.log(userObject1); // Output: { name: 'SAMMY', email: 'SAMMY@EXAMPLE.COM', plan: 'PRO' }
+
+// JSON.stringify() - takes a JavaScript object and transforms it into a JSON string.
+
+let userObject2 = {
+    name: "Sammy",
+    email: "sammy@example.com",
+    plan: "Pro"
+  };
+  
+let userString3 = JSON.stringify(userObject2);
+  
+console.log(userString3); // Output: {"name":"Sammy","email":"sammy@example.com","plan":"Pro"}
+
+// 11. Async Javascript 
--- ReadMe.txt.txt
+++ ReadMe.txt.txt
@@ -1,20 +1,20 @@
 Topics to prepare for interview:
 
-1.Fundamentals: Variables, data types, operators.
-2.Closures: Master this concept, as it's often tested in interviews.
-3.Functions: Dive deep into functions, understanding scope and 'this' keyword.
-4.ES6 Features: Arrow Functions, destructuring, spread / rest operators and template literals.
-5.Async Javascript: Promises and async/await for handling asynchronous operations.
-6.DOM Manipulation: Know how to interact with the Document Object Model (DOM) effectively.
-7.Event Handling: Learn about event listeners, event delegationand the event object.
-8.Prototypes and Classes: Understand Javascript's prototypal inheritance and ES6 Classes.
-9.Module Systems: Explore CommonJS, AMD, and ES6 modules.
-10.AJAX and Fetch API: Learn how to make asynchronous HTTP requests.
-11.Design Patterns: Know the most common patterns like Singleton, Observer, and Module.
-12.JSON: Understand JSON parsing and stringify.
-13.Error Handling: Learn how to handle errors gracefullt with try...catch.
-14.Data Structures: Arrays, Objects, maps, and sets are essential.
-15.Functional Programming: Concepts like map, filter and reduce are valuable.
+1.Fundamentals: Variables, data types, operators. - Done
+2.Closures: Master this concept, as it's often tested in interviews. - Done
+3.Functions: Dive deep into functions, understanding scope and 'this' keyword. - Done
+4.ES6 Features: Arrow Functions, destructuring, spread / rest operators and template literals.  - Done
+5.Data Structures: Arrays, Objects, maps, and sets are essential. - Done
+6.Functional Programming: Concepts like map, filter and reduce are valuable. - Done
+7.Error Handling: Learn how to handle errors gracefully with try...catch.
+8.JSON: Understand JSON parsing and stringify. - Done
+9.Prototypes and Classes: Understand Javascript's prototypal inheritance and ES6 Classes.
+10.Module Systems: Explore CommonJS, AMD, and ES6 modules.
+11.AJAX and Fetch API: Learn how to make asynchronous HTTP requests.
+12.Async Javascript: Promises and async/await for handling asynchronous operations. - Inprogress
+13.DOM Manipulation: Know how to interact with the Document Object Model (DOM) effectively.
+14.Event Handling: Learn about event listeners, event delegationand the event object.
+15.Design Patterns: Know the most common patterns like Singleton, Observer, and Module.
 16.Build Tools: Grasp  Webpack and Babel for bundling and transpiling.
 17.Testing: Explore testing frameworks like Jest and Mocha.
 18.Debugging: Proviciency with browser developer tools is a must.
